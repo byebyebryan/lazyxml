f0dfc2eeb57d5bc64b12215ecb80b1a8065198e2
byebyebryan <byebyebryan@gmail.com>
2015-02-13 01:57:47 -0500

    returned to the simpler wrapper class approach - now we only need a single header file! - got rid of the cluttered comments

--- LazyXML/LazyXML.h
+++ LazyXML/LazyXML.h
@@ -17,256 +17,223 @@ simple & stupid c++ auto xml serialization based on tinyxml2
 #include <assert.h>
 #include "tinyxml2.h"
 
-//************************************
-// Begin class declaration
-//  -declare basic structure for singleton
-//  -declare init()
-//************************************
-#define LAZY_XML_BEGIN_DECLARATION(className) \
-class className : public LazyXML::Base { \
-	private: className() {} \
-	static className * instance; \
-	public: static className * getInstance(); \
-	static void destoryInstance(); \
-	protected: void init() override;
+#define LAZY_XML_VAR(var_class, var_type, var_name) \
+	private: class LazyXML_##var_name { \
+	public: \
+		std::string name_; \
+		var_type value_; \
+		LazyXML_##var_name () : name_(#var_name) { \
+			value_ = var_type(); \
+			var_class::reading_funcs()[#var_name] = std::bind(&var_class::VarType<var_type>::reader, std::placeholders::_1, &value_); \
+			var_class::writing_funcs()[#var_name] = std::bind(&var_class::VarType<var_type>::writer, std::placeholders::_1, #var_name, std::cref(value_)); \
+		} \
+	}; \
+	private: LazyXML_##var_name var_name##_; \
+	public: var_type var_name() {return var_name##_.value_;} \
+	public: void set_##var_name(const var_type & value) {var_name##_.value_ = value;}
 
-//************************************
-// End class declaration
-//************************************
-#define LAZY_XML_END_DECLARATION };
+namespace lazyxml {
+	using namespace tinyxml2;
 
-//************************************
-// Begin class implementation
-//	-implement basic function for singleton
-//	-begin implementation of init()
-//************************************
-#define LAZY_XML_BEGIN_IMPLEMENTATION(className) \
-className * className::instance = nullptr; \
-className * className::getInstance() { \
-	if (!instance) {instance = new className(); instance->init();} \
-	return instance; \
-} \
-void className::destoryInstance() { \
-	if (instance) {delete instance; instance = nullptr;} \
-} \
-void className::init() {
-
-//************************************
-// End class implementation
-//************************************
-#define LAZY_XML_END_IMPLEMENTATION }
-
-namespace LazyXML
-{
-	class Base
-	{
-	protected:
-		//************************************
-		// map of var name to reading/writing functions
-		//  -function should be already binded to storage of var
-		//************************************
-		std::unordered_map<std::string, std::function<void(tinyxml2::XMLElement *)>> readingFuncs;
-		std::unordered_map<std::string, std::function<void(tinyxml2::XMLElement *)>> writingFuncs;
-
-		//************************************
-		// FullName:  LazyXML::Base::init
-		// Access:    virtual protected 
-		// Returns:   void
-		//
-		// Summary:   populate read/write function maps
-		//************************************
-		virtual void init() = 0;
-
+	template<typename T>
+	class Base {
 	public:
-
-		//************************************
-		// FullName:  LazyXML::Base::readFromFile
-		// Access:    public 
-		// Returns:   void
-		// Parameter: const std::string & fileName
-		//
-		// Summary:   read from file
-		//************************************
-		void readFromFile(const std::string & fileName);
-
-		//************************************
-		// FullName:  LazyXML::Base::readFromBuffer
-		// Access:    public 
-		// Returns:   void
-		// Parameter: const std::string & buffer
-		//
-		// Summary:   read from buffer
-		//************************************
-		void readFromBuffer(const std::string & buffer);
-		
-		//************************************
-		// FullName:  LazyXML::Base::writeToBuffer
-		// Access:    public 
-		// Returns:   void
-		// Parameter: std::string & buffer
-		//
-		// Summary:   write to buffer
-		//************************************
-		void writeToBuffer(std::string & buffer);
-
-
-		//************************************
-		// FullName:  LazyXML::Base::writeToFile
-		// Access:    public 
-		// Returns:   void
-		// Parameter: const std::string & fileName
-		//
-		// Summary:   write to file
-		//************************************
-		void writeToFile(const std::string & fileName);
-	};
-
-	//************************************
-	// container struct to provide read/write function for generic type T
-	//  -default implementation block execution
-	//  -specialize T to override
-	//************************************
-	template<typename T>
-	struct VarType
-	{
-		static void reader(tinyxml2::XMLElement * xmlElement, T & value)
-		{
-			std::cerr << "missing XML reading function for value type : " << typeid(T).name() << std::endl;
-			assert(false);
+		//singleton implementation
+		static T* GetInstance() {
+			static T* instance = new T();
+			return instance;
 		}
 
-		static void writer(tinyxml2::XMLElement * xmlElement, const std::string & name, const T & value)
-		{
-			std::cerr << "missing XML writing function for value type : " << typeid(T).name() << std::endl;
-			assert(false);
+		//stream in from xml
+		void ReadFromFile(const std::string & file_name) {
+			std::ifstream fileStream(file_name);
+			std::stringstream stringBuffer;
+			stringBuffer << fileStream.rdbuf();
+			ReadFromBuffer(stringBuffer.str());
 		}
-	};
-
-	//************************************
-	// partially specialized read/write for container type std::vector<T>
-	//  -calls fully specialized read/write for type T
-	//************************************
-	template<typename T>
-	struct VarType < std::vector<T> >
-	{
-		static void reader(tinyxml2::XMLElement * xmlElement, std::vector<T> * value)
-		{
-			tinyxml2::XMLElement * childElement = xmlElement->FirstChildElement();
-			while (childElement)
+		void ReadFromBuffer(const std::string & buffer) {
+			XMLDocument * xmlDoc = new XMLDocument();
+			xmlDoc->Parse(buffer.c_str());
+			XMLElement * xmlNode = xmlDoc->RootElement()->FirstChildElement();
+			while (xmlNode)
 			{
-				T childValue;
-				VarType<T>::reader(childElement, &childValue);
-				value->push_back(childValue);
-				childElement = childElement->NextSiblingElement();
+				auto it = reading_funcs().find(xmlNode->Name());
+				if (it != reading_funcs().end())
+				{
+					it->second(xmlNode);
+				}
+				xmlNode = xmlNode->NextSiblingElement();
 			}
+			delete xmlDoc;
 		}
 
-		static void writer(tinyxml2::XMLElement * xmlElement, const std::string & name, const std::vector<T> & value)
-		{
-			tinyxml2::XMLElement * newElement = xmlElement->GetDocument()->NewElement(name.c_str());
-			for (auto & item : value)
+		//stream out to xml
+		void WriteToBuffer(std::string & buffer) {
+			XMLDocument * xmlDoc = new XMLDocument();
+			XMLDeclaration * xmlDecl = xmlDoc->NewDeclaration();
+			xmlDoc->InsertFirstChild(xmlDecl);
+			XMLElement * rootElement = xmlDoc->NewElement("root");
+			xmlDoc->InsertEndChild(rootElement);
+
+			for (auto & it : writing_funcs())
 			{
-				VarType<T>::writer(newElement, "item", item);
+				it.second(rootElement);
 			}
-			xmlElement->InsertEndChild(newElement);
+
+			XMLPrinter xmlPrinter;
+			xmlDoc->Print(&xmlPrinter);
+			buffer = std::string(xmlPrinter.CStr());
+		}
+		void WriteToFile(const std::string & file_name) {
+			std::string buffer;
+			WriteToBuffer(buffer);
+			std::ofstream fileStream(file_name);
+			fileStream << buffer;
+		}
+
+	protected:
+		// container struct to provide read/write function for generic type T
+		//  -default implementation block execution
+		//  -specialize T to override
+		template<typename T>
+		struct VarType
+		{
+			static void reader(tinyxml2::XMLElement * xmlElement, T & value)
+			{
+				std::cerr << "missing XML reading function for value type : " << typeid(T).name() << std::endl;
+				assert(false);
+			}
+
+			static void writer(tinyxml2::XMLElement * xmlElement, const std::string & name, const T & value)
+			{
+				std::cerr << "missing XML writing function for value type : " << typeid(T).name() << std::endl;
+				assert(false);
+			}
+		};
+
+		// partially specialized read/write for container type std::vector<T>
+		//  -calls fully specialized read/write for type T
+		template<typename T>
+		struct VarType < std::vector<T> >
+		{
+			static void reader(tinyxml2::XMLElement * xmlElement, std::vector<T> * value)
+			{
+				tinyxml2::XMLElement * childElement = xmlElement->FirstChildElement();
+				while (childElement)
+				{
+					T childValue;
+					VarType<T>::reader(childElement, &childValue);
+					value->push_back(childValue);
+					childElement = childElement->NextSiblingElement();
+				}
+			}
+
+			static void writer(tinyxml2::XMLElement * xmlElement, const std::string & name, const std::vector<T> & value)
+			{
+				tinyxml2::XMLElement * newElement = xmlElement->GetDocument()->NewElement(name.c_str());
+				for (auto & item : value)
+				{
+					VarType<T>::writer(newElement, "item", item);
+				}
+				xmlElement->InsertEndChild(newElement);
+			}
+		};
+
+		// fully specialized read/write for bool
+		template<>
+		struct VarType < bool >
+		{
+			static void reader(tinyxml2::XMLElement * xmlElement, bool * value)
+			{
+				xmlElement->QueryBoolText(value);
+			}
+
+			static void writer(tinyxml2::XMLElement * xmlElement, const std::string & name, const bool & value)
+			{
+				tinyxml2::XMLElement * newElement = xmlElement->GetDocument()->NewElement(name.c_str());
+				newElement->SetText(value);
+				xmlElement->InsertEndChild(newElement);
+			}
+		};
+
+		// fully specialized read/write for int
+		template<>
+		struct VarType < int >
+		{
+			static void reader(tinyxml2::XMLElement * xmlElement, int * value)
+			{
+				xmlElement->QueryIntText(value);
+			}
+
+			static void writer(tinyxml2::XMLElement * xmlElement, const std::string & name, const int & value)
+			{
+				tinyxml2::XMLElement * newElement = xmlElement->GetDocument()->NewElement(name.c_str());
+				newElement->SetText(value);
+				xmlElement->InsertEndChild(newElement);
+			}
+		};
+
+		// fully specialized read/write for float
+		template<>
+		struct VarType < float >
+		{
+			static void reader(tinyxml2::XMLElement * xmlElement, float * value)
+			{
+				xmlElement->QueryFloatText(value);
+			}
+
+			static void writer(tinyxml2::XMLElement * xmlElement, const std::string & name, const float & value)
+			{
+				tinyxml2::XMLElement * newElement = xmlElement->GetDocument()->NewElement(name.c_str());
+				newElement->SetText(value);
+				xmlElement->InsertEndChild(newElement);
+			}
+		};
+
+		// fully specialized read/write for double
+		template<>
+		struct VarType < double >
+		{
+			static void reader(tinyxml2::XMLElement * xmlElement, double * value)
+			{
+				xmlElement->QueryDoubleText(value);
+			}
+
+			static void writer(tinyxml2::XMLElement * xmlElement, const std::string & name, const double & value)
+			{
+				tinyxml2::XMLElement * newElement = xmlElement->GetDocument()->NewElement(name.c_str());
+				newElement->SetText(value);
+				xmlElement->InsertEndChild(newElement);
+			}
+		};
+
+		// fully specialized read/write for std::string
+		template<>
+		struct VarType < std::string >
+		{
+			static void reader(tinyxml2::XMLElement * xmlElement, std::string * value)
+			{
+				*value = xmlElement->GetText();
+			}
+
+			static void writer(tinyxml2::XMLElement * xmlElement, const std::string & name, const std::string & value)
+			{
+				tinyxml2::XMLElement * newElement = xmlElement->GetDocument()->NewElement(name.c_str());
+				newElement->SetText(value.c_str());
+				xmlElement->InsertEndChild(newElement);
+			}
+		};
+
+		//readingd/writing function maps
+		static std::unordered_map<std::string, std::function<void(tinyxml2::XMLElement *)>> & reading_funcs() {
+			static std::unordered_map<std::string, std::function<void(tinyxml2::XMLElement *)>> funcs;
+			return funcs;
+		};
+		static std::unordered_map<std::string, std::function<void(tinyxml2::XMLElement *)>> & writing_funcs() {
+			static std::unordered_map<std::string, std::function<void(tinyxml2::XMLElement *)>> funcs;
+			return funcs;
 		}
 	};
-
-	//************************************
-	// fully specialized read/write for bool
-	//************************************
-	template<>
-	struct VarType < bool >
-	{
-		static void reader(tinyxml2::XMLElement * xmlElement, bool * value)
-		{
-			xmlElement->QueryBoolText(value);
-		}
-
-		static void writer(tinyxml2::XMLElement * xmlElement, const std::string & name, const bool & value)
-		{
-			tinyxml2::XMLElement * newElement = xmlElement->GetDocument()->NewElement(name.c_str());
-			newElement->SetText(value);
-			xmlElement->InsertEndChild(newElement);
-		}
-	};
-
-	//************************************
-	// fully specialized read/write for int
-	//************************************
-	template<>
-	struct VarType < int >
-	{
-		static void reader(tinyxml2::XMLElement * xmlElement, int * value)
-		{
-			xmlElement->QueryIntText(value);
-		}
-
-		static void writer(tinyxml2::XMLElement * xmlElement, const std::string & name, const int & value)
-		{
-			tinyxml2::XMLElement * newElement = xmlElement->GetDocument()->NewElement(name.c_str());
-			newElement->SetText(value);
-			xmlElement->InsertEndChild(newElement);
-		}
-	};
-
-	//************************************
-	// fully specialized read/write for float
-	//************************************
-	template<>
-	struct VarType < float >
-	{
-		static void reader(tinyxml2::XMLElement * xmlElement, float * value)
-		{
-			xmlElement->QueryFloatText(value);
-		}
-
-		static void writer(tinyxml2::XMLElement * xmlElement, const std::string & name, const float & value)
-		{
-			tinyxml2::XMLElement * newElement = xmlElement->GetDocument()->NewElement(name.c_str());
-			newElement->SetText(value);
-			xmlElement->InsertEndChild(newElement);
-		}
-	};
-
-	//************************************
-	// fully specialized read/write for double
-	//************************************
-	template<>
-	struct VarType < double >
-	{
-		static void reader(tinyxml2::XMLElement * xmlElement, double * value)
-		{
-			xmlElement->QueryDoubleText(value);
-		}
-
-		static void writer(tinyxml2::XMLElement * xmlElement, const std::string & name, const double & value)
-		{
-			tinyxml2::XMLElement * newElement = xmlElement->GetDocument()->NewElement(name.c_str());
-			newElement->SetText(value);
-			xmlElement->InsertEndChild(newElement);
-		}
-	};
-
-	//************************************
-	// fully specialized read/write for std::string
-	//************************************
-	template<>
-	struct VarType < std::string >
-	{
-		static void reader(tinyxml2::XMLElement * xmlElement, std::string * value)
-		{
-			*value = xmlElement->GetText();
-		}
-
-		static void writer(tinyxml2::XMLElement * xmlElement, const std::string & name, const std::string & value)
-		{
-			tinyxml2::XMLElement * newElement = xmlElement->GetDocument()->NewElement(name.c_str());
-			newElement->SetText(value.c_str());
-			xmlElement->InsertEndChild(newElement);
-		}
-	};
-	
 }
 
 #endif // _LAZY_XML_H
--- LazyXML/example.cpp
+++ LazyXML/example.cpp
@@ -2,52 +2,32 @@
 // An example of how to use LazyXML
 //************************************
 
-#include <iostream>
+//Better to put these in a header file
 #include "LazyXML.h"
 
+//Extend from lazy::Base class
+class ExampleXML : public lazyxml::Base < ExampleXML > {
+	//List variables
+	LAZY_XML_VAR(ExampleXML, bool, TestVar0)
+	LAZY_XML_VAR(ExampleXML, int, TestVar1)
+	LAZY_XML_VAR(ExampleXML, float, TestVar2)
+	LAZY_XML_VAR(ExampleXML, double, TestVar3)
+	LAZY_XML_VAR(ExampleXML, std::vector<int>, TestVar4)
+};
 
-//************************************
-// this block should be put into a header file
-#define LAZY_XML_DECLARATION	//set the flag for header
-#include "LazyXML_Def.h"	//include the macro helper file (could copy the content here too)
-
-LAZY_XML_BEGIN(TestXML)		//begin class declaration
-
-//declare all the vars
-LAZY_XML_VAR(bool, TestVar0)
-LAZY_XML_VAR(int, TestVar1)
-LAZY_XML_VAR(float, TestVar2)
-LAZY_XML_VAR(double, TestVar3)
-LAZY_XML_VAR(std::vector<int>, TestVar4)
-
-LAZY_XML_END	//end class declaration
-//
-//************************************
-
-
-//************************************
-// this block should be put into a source file
-#undef LAZY_XML_DECLARATION		//unset the flag for header
-#include "LazyXML_Def.h"	//include the macro helper file (could copy the content here too)
-
-//copied over from the header file
-LAZY_XML_BEGIN(TestXML)
-LAZY_XML_VAR(bool, TestVar0)
-LAZY_XML_VAR(int, TestVar1)
-LAZY_XML_VAR(float, TestVar2)
-LAZY_XML_VAR(double, TestVar3)
-LAZY_XML_VAR(std::vector<int>, TestVar4)
-LAZY_XML_END
-//
-//************************************
-
+#include <iostream>
 
 int main(int argc, const char ** argv)
 {
-	TestXML::getInstance()->readFromFile("example.xml");	//read from file
-	//TestXML::getInstance()->get_TestVar0();	//get a var
-	//TestXML::getInstance()->set_TestVar2(0);	//set a var
+	//read from file
+	ExampleXML::GetInstance()->ReadFromFile("example.xml");
+
+	//getters and setters
+	bool var_0 = ExampleXML::GetInstance()->TestVar0();
+	ExampleXML::GetInstance()->set_TestVar1(1);
+
+	//write to buffer
 	std::string buffer;
-	TestXML::getInstance()->writeToBuffer(buffer);		//write to buffer
+	ExampleXML::GetInstance()->WriteToBuffer(buffer);
 	std::cout << buffer << std::endl;
 }
